85afb2be9c51bc84e80d7737c5d027a786aec448
vamsikrishna515 <vloya1@asu.edu>
2022-10-27 22:47:50 -0700

    modified code to write file

--- urinals.java
+++ urinals.java
@@ -5,33 +5,45 @@ public class urinals {
      * @param args
      */
     public static void main(String[] args) throws IOException {
-        String [] stringsArray = null;
-        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\vloya1\\Desktop\\urinal.dat"))) {
-            List<String> strings = new ArrayList<>();
-            br.lines().forEach(c -> strings.add(c));
-            stringsArray = strings.toArray(new String[strings.size()]);
-        }
 
-        int[] result = new int[stringsArray.length];
-        int i=0;
-        for(String eachString: stringsArray)
+
+        int y = 0;
+
+        while(y !=1)
         {
-           result[i++] = counter(eachString);
-        }
-        String fileName = "rule";
+            System.out.println("Pick an option:\n 1 for Keyboard Input \n 2 for File Input \n 3 Exit\n");
+            Scanner sc = new Scanner(System.in);
+            int choice = Integer.parseInt(sc.nextLine());
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("Enter a string:");
+                    String input = sc.nextLine();
+                    System.out.println("Result: " + counter(input) + "\n");
+                    break;
+                case 2:
+                    String[] stringsArray = readFile();
+                    int[] result = new int[stringsArray.length];
+                    for(int i=0;i< result.length;i++)
+                    {
+                        result[i] = counter(stringsArray[i]);
+                    }
+                    writeFile(result);
+                    System.out.println("Results saved in file!\n");
+                    break;
 
-        File file = new File(fileName+ ".txt");
-        int count = 0;
-        while(!file.createNewFile())
-        {
-            count++;
-            file = new File(fileName+ String.valueOf(count) +".txt");
+                case 3: y=1; break;
+            }
         }
 
-        
+
+
+
+
     }
     public  static int counter(String ques)
     {
+        if(ques.contains("11")) return -1;
         int prev = 0;
         int count = 0;
 
@@ -69,5 +81,40 @@ public class urinals {
         return count;
     }
 
+    public static String[] readFile() throws IOException
+    {
+        String [] stringsArray = null;
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\vloya1\\Desktop\\urinal.dat"))) {
+            List<String> strings = new ArrayList<>();
+            br.lines().forEach(c -> strings.add(c));
+            stringsArray = strings.toArray(new String[strings.size()]);
+        }
+        return stringsArray;
+    }
+
+    public static void writeFile (int[] result) throws IOException
+    {
+        String fileName = "rule";
+
+        File file = new File(fileName+ ".txt");
+        int count = 0;
+        while(!file.createNewFile())
+        {
+            count++;
+            file = new File(fileName+ String.valueOf(count) +".txt");
+        }
+
+        String content = "";
+
+        for(int eachResult : result)
+        {
+            content+=String.valueOf(eachResult)+"\n";
+
+        }
+
+        FileWriter fw = new FileWriter(file);
+        fw.write(content);
+        fw.close();
+    }
 }
 
