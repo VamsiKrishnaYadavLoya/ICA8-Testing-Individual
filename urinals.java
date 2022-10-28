26f98b3f99b29ab15d6a880804fbc85dada0da3e
vamsikrishna515 <vloya1@asu.edu>
2022-10-27 21:05:03 -0700

    urinals.java added

---
+++ urinals.java
@@ -0,0 +1,12 @@
+public class urinals {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Not yet implemented");
+
+        System.out.println("Author - Vamsi Krishna Yadav");
+
+    }
+}
